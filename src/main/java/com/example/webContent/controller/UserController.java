40e9c147913e1c0f58c653c71e820267e489e6b7
itDarashukD <itdarashukd@gmail.com>
2020-12-25 20:02:01 +0300

    Создали ограничение доступа для аользователей не явл.Админом к списку юзеров  проверка доступа поьзователем @PreAuthorize("hasAuthority('ADMIN')")

---
+++ src/main/java/com/example/webContent/controller/UserController.java
@@ -0,0 +1,62 @@
+package com.example.webContent.controller;
+
+import com.example.webContent.accesDB.Role;
+import com.example.webContent.accesDB.User;
+import com.example.webContent.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.*;
+import java.util.stream.Collectors;
+@Controller
+@RequestMapping("/user")
+ @PreAuthorize("hasAuthority('ADMIN')")             //возможности введения доступа только Админа
+public class UserController {                     //этот К создан для возможности введения доступа только Админа
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public String userList(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+
+        return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+
+        return "userEdit";
+    }
+
+    @PostMapping
+    public String userSave(
+            @RequestParam String username,
+            @RequestParam Map<String, String> form,
+            @RequestParam("userId") User user
+    ) {
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userRepository.save(user);
+
+        return "redirect:/user";
+    }
+}
--- src/main/java/com/example/webContent/securityWeb/WebSecurityConfig.java
+++ src/main/java/com/example/webContent/securityWeb/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import com.example.webContent.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +16,7 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+ @EnableGlobalMethodSecurity(prePostEnabled = true)//разрешаем импользование @ для проверки доступа поьзователем @PreAuthorize("hasAuthority('ADMIN')")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
    @Autowired
    private UserService userService;
